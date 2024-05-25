2749f1a44cd803f8da102d9c38cb89822591cf30
Animace <eloquent.javascript@proton.me>
2024-05-25 13:09:52 +0300

    done with mongoose conn

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,11 +1,16 @@
 const asyncHandler = require ('express-async-Handler');
 
+const Goal = require('../models/goalModel')
+
 //@desc Get goals 
 // @route Get/api/goals
 //@access Private
 
 const getGoals = asyncHandler (async (req, res) =>{
-    res.status(200).json ({message:'Get goals'});
+
+    const goals = await Goal.find()
+
+    res.status(200).json (goals);
 })
 //@desc Post goals 
 // @route POST/api/goals
@@ -17,22 +22,47 @@ const setGoal = asyncHandler (async (req, res) =>{
     throw new Error('Please add a text field')
    }
 
+   const goal = await Goal.create({
+    text:req.body.text
+   })
 
-    res.status(200).json ({message:'Set goals'});
+
+    res.status(200).json (goal);
 })
 //@desc Update goals 
 // @route PUT/api/goals/:id
 //@access Private
 
 const updateGoal = asyncHandler ( async (req, res) =>{
-    res.status(200).json({message:`Update Goals ${req.params.id}`});
+    const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const updateGoal = await Goal.findByIdAndUpdate(req.params.id, req.body, {new:true,})
+
+
+    res.status(200).json(updateGoal);
 })
 //@desc delete goals 
 // @route Delete/api/goals:/id
 //@access Private
 
 const deleteGoal = asyncHandler ( async (req, res) =>{
-    res.status(200).json({message:`Delete Goals ${req.params.id}`});
+
+const goal = await Goal.findById(req.params.id)
+
+    if(!goal){
+        res.status(400)
+        throw new Error('Goal not found')
+    }
+
+    const deleteGoal = await Goal.findByIdAndDelete(req.params.id, req.body, {new:true,})
+
+
+    res.status(200).json(deleteGoal);
 })
 
 
